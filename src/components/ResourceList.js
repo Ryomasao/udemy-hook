66f3fdc55cee9e8a90ae807e872bf88e9b195d17
ryoji.yamauchi <ryoji_yamauchi@waku-2.com>
2019-01-28 15:19:12 +0900

    UseEffect

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,30 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect} from 'react'
 import axios from 'axios'
 
-class ResourceList extends React.Component {
-  state = { resources: [] }
+const ResourceList = ({ resource }) => {
+  const [resources, setResources] = useState([])
 
-  async componentDidMount() {
-    const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`)
-    this.setState({resources: response.data})
-  }
+  //const fetchResource = async resource => {
+  //  const response = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`)
+  //  setResources(response.data)
+  //}
 
-  async componentDidUpdate(prevProps) {
-    if (prevProps.resource !== this.props.resource) {
-      const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`)
-      this.setState({resources: response.data})
-    }
-  }
+  // 第二引数の値が変わった場合にupdateとみなされる
+  //useEffect(() => {
+  //  fetchResource(resource)
+  //}, [resource])
 
-  renderList() {
-    this.state.resources.map()
-  }
+  // これでもいい
+  useEffect(() => {
+    ( async resource =>{
+      const response = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`)
+      setResources(response.data)
+    })(resource)
+  }, [resource])
 
-  render() {
-    return (
-      <div>{this.state.resources.length}</div>
-    );
-  };
+  return (
+
+    <ul>{resources.map(record => <li key={record.id}>{record.title}</li>)}</ul>
+  );
 }
 
 export default ResourceList;
